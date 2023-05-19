7be610bf6f7758c4c0fab59d5c72ec102ea7aa2b
ehfthxqlqla <0909aiden@naver.com>
2023-05-19 16:40:57 +0900

    정보 입력 페이지 생성

--- src/front-end/js/index.js
+++ src/front-end/js/index.js
@@ -17,9 +17,8 @@ const queryString = (target) => {
     return b;
 }
 
-const step1 = `
-
-`
+const step1WelcomeText = document.querySelector(".css-step1-welcome"),
+step1SubWelcomeText = document.querySelector(".css-step1-subwelcome")
 
 const container = document.querySelector(".container")
 
@@ -35,13 +34,13 @@ window.onload = () => {
     if (!qs?.step) {
         console.log(`mainpage`)
 
-        container.innerHTML
-
-        document.querySelector(".css-step1-welcome").style.transform = `translateY(0px)`
-        document.querySelector(".css-step1-subwelcome").style.transform = `translateY(0px)`
+        step1WelcomeText.style.transform = `translateY(15px)`
+        step1SubWelcomeText.style.transform = `translateY(0px)`
         document.querySelector(".css-step1-termsbox").style.transform = `translate(-50%, -50%)`
     }
 }
+
+// window.onbeforeunload = () => { return "아직 테스트가 완료되지 않았습니다. 정말로 나가시겠습니까?" }
 
 const nextButton1 = document.querySelector(".js-next-step1"),
 termsAcceptButton = document.querySelector(".js-accept-terms"),
@@ -53,4 +52,16 @@ termsAcceptButton.addEventListener("click", function() {
 
 termsDenyButton.addEventListener("click", function() {
     nextButton1.disabled = true
+})
+
+nextButton1.addEventListener("click", function() {
+    document.querySelector(".css-step1-termsbox").style.opacity = 0
+
+    document.querySelector(".css-step2-information").style.display = `block`
+    setTimeout(() => {
+        document.querySelector(".css-step1-termsbox").remove()
+        document.querySelector(".css-step2-information").style.transform = `translateX(0px)`
+        step1WelcomeText.innerText = `테스트에 필요한 정보를 입력하세요.`
+        step1SubWelcomeText.innerText = `개인정보는 수집 목적 외에 사용되지 않습니다.`
+    }, 2000);
 })
