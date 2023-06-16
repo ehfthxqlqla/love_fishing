da2c10040e25345b55704147979619008ac42bf2
ehfthxqlqla <0909aiden@naver.com>
2023-06-16 15:42:45 +0900

    디자인 다 뜯어고쳤다 김민준 십새끼

--- src/front-end/js/index.js
+++ src/front-end/js/index.js
@@ -17,6 +17,20 @@ const queryString = (target) => {
     return b;
 }
 
+// input 엘리먼트를 선택합니다.
+const inputElement = document.querySelectorAll('.css-step2-information-input-phone');
+
+for (const element of inputElement) {
+    element.addEventListener("keyup", function(e) {
+        const test = () => {
+            e.target.value = e.target.value
+            .replace(/[^0-9]/g, '')
+            .replace(/(^02.{0}|^01.{1}|[0-9]{3,4})([0-9]{3,4})([0-9]{4})/g, "$1-$2-$3");
+        }
+        test();
+    })
+}
+
 const step1WelcomeText = document.querySelector(".css-step1-welcome"),
 step1SubWelcomeText = document.querySelector(".css-step1-subwelcome")
 
@@ -43,16 +57,8 @@ window.onload = () => {
 // window.onbeforeunload = () => { return "아직 테스트가 완료되지 않았습니다. 정말로 나가시겠습니까?" }
 
 const nextButton1 = document.querySelector(".js-next-step1"),
-termsAcceptButton = document.querySelector(".js-accept-terms"),
-termsDenyButton = document.querySelector(".js-deny-terms")
-
-termsAcceptButton.addEventListener("click", function() {
-    nextButton1.disabled = false
-})
-
-termsDenyButton.addEventListener("click", function() {
-    nextButton1.disabled = true
-})
+termsCheckButton = document.querySelector(".js-terms-necessary"),
+termsCheckBox = document.querySelector(".js-necessary")
 
 nextButton1.addEventListener("click", function() {
     document.querySelector(".css-step1-termsbox").style.opacity = 0
@@ -64,4 +70,23 @@ nextButton1.addEventListener("click", function() {
         step1WelcomeText.innerText = `테스트에 필요한 정보를 입력하세요.`
         step1SubWelcomeText.innerText = `개인정보는 수집 목적 외에 사용되지 않습니다.`
     }, 2000);
+})
+
+termsCheckButton.addEventListener("click", function() {
+    switch (termsCheckBox.checked) {
+        case true:
+            nextButton1.disabled = false
+            break;
+        case false:
+            nextButton1.disabled = true
+            break;
+    }
+})
+
+$(".js-terms-box-show").click(function() {
+    $(".css-terms-text").slideDown()
+})
+
+$(".js-terms-box-fishing").click(function() {
+    alert(`그걸 왜 동의함`)
 })
